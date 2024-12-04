918224caa4c7a84f194de04fb8453b472ae7ec5c
sojfowpw <alexandra.plisova@gmail.com>
2024-12-05 01:03:05 +0700

    create eating

--- animals.cpp
+++ animals.cpp
@@ -3,45 +3,95 @@
 using namespace std;
 
 class Animal { // родительский класс животных 
-    public: //
+    public:
     int x, y;
-    Animal(int a_x, int a_y, int a_direction, int a_k) : x(a_x), y(a_y), direction(a_direction), k(a_k) {}
+    Animal(int x, int y, int direction, int k) : x(x), y(y), direction(direction), k(k) {}
     virtual ~Animal() {} // деструктор
 
-    void moving(int M, int N) {
-        static int moveCount = 0;
-        if (moveCount % k == 0 && moveCount != 0) {
-            direction = (direction + 1) % 4;
-        }
-        switch (direction) {
-            case 0:
-                y = (y - 1 + M) % M;
-                break;
-            case 1:
-                x = (x + 1) % N;
-                break;
-            case 2:
-                y = (y + 1) % M;
-                break;
-            case 3:
-                x = (x - 1 + N) % N;
-                break;
-        }
-        moveCount++;
-        cout << x << ", " << y << endl;
-    }
+    virtual void moving(int M, int N) = 0; // движение
+    virtual void eating(Animal& rabbit) = 0; // питание
 
-    private:
-    //int x, y;
+    protected:
     int direction;
     int k;
 };
 
-void printField(int M, int N, Animal& a) {
+class Predator : public Animal { // класс хищник
+    public:
+    Predator(int x, int y, int direction, int k, int hungerThreshold) : Animal(x, y, direction, k), hunger(0),
+    hungerThreshold(hungerThreshold) {}
+
+    void moving(int M, int N) override { // движение
+        static int moveCount = 0; // счётчик ходов
+        if (moveCount % k == 0 && moveCount != 0) {
+            direction = (direction + 1) % 4; // смена направления
+        }
+        switch (direction) {
+            case 0: // вверх
+                y = (y - 2 + M) % M;
+                break;
+            case 1: // вправо
+                x = (x + 2) % N;
+                break;
+            case 2: // вниз
+                y = (y + 2) % M;
+                break;
+            case 3: // влево
+                x = (x - 2 + N) % N;
+                break;
+        }
+        moveCount++;
+    }
+
+    void eating(Animal& rabbit) override {
+        if (x == rabbit.x && y == rabbit.y) {
+            hunger++;
+        }
+        cout << hunger << endl;
+    }
+
+    private:
+    int hunger; // текущее насыщение
+    int hungerThreshold; // максимальное насыщение
+};
+
+class Victim : public Animal { // класс жертва
+    public:
+    Victim(int x, int y, int direction, int k) : Animal(x, y, direction, k) {}
+
+    void moving(int M, int N) override { // движение
+        static int moveCount = 0; // счётчик ходов
+        if (moveCount % k == 0 && moveCount != 0) {
+            direction = (direction + 1) % 4; // смена направления
+        }
+        switch (direction) {
+            case 0: // вверх
+                y = (y - 1 + M) % M;
+                break;
+            case 1: // вправо
+                x = (x + 1) % N;
+                break;
+            case 2: // вниз
+                y = (y + 1) % M;
+                break;
+            case 3: // влево
+                x = (x - 1 + N) % N;
+                break;
+        }
+        moveCount++;
+    }
+
+    void eating(Animal& rabbit) override {}
+};
+
+void printField(int M, int N, Animal& wolf, Animal& rabbit) { // вывод поля
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
-            if (i == a.y && j == a.x) {
-                cout << "A";
+            if (i == wolf.y && j == wolf.x) {
+                cout << "w";
+            }
+            else if (i == rabbit.y && j == rabbit.x) {
+                cout << "r";
             }
             else {
                 cout << "*";
@@ -54,8 +104,9 @@ void printField(int M, int N, Animal& a) {
 int main() {
     int M, N;//, T; // размеры поля и количество ходов
     cin >> M >> N; // M - длина, N - ширина
-    Animal a(0, 0, 1, 2);
-    printField(M, N, a);
+    Predator wolf(0, 0, 1, 100, 2);
+    Victim rabbit(0, 0, 1, 100);
+    printField(M, N, wolf, rabbit);
     cout << endl << endl;
     // int R, W; // начальное количество жертв и хищников
     // int x, y, d, k; // x, y - координаты ({0, 0} - левый верхний угол), d - начальное направление, k - ходы перед сменой направления
@@ -66,8 +117,10 @@ int main() {
     //     cin >> x >> y >> d >> k;
     // }
     for (int i = 0; i < 7; i++) {
-        a.moving(M, N);
-        printField(M, N, a);
+        wolf.moving(M, N);
+        rabbit.moving(M, N);
+        wolf.eating(rabbit);
+        printField(M, N, wolf, rabbit);
         cout << endl << endl;
     }
     //delete &a;
